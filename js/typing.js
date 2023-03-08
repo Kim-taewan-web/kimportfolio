d7aaf26d2015729b5bac7201450b036bf2106352
Kim-taewan-web <122958014+Kim-taewan-web@users.noreply.github.com>
2023-03-08 16:36:13 +0900

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,72 @@
+const $text = document.querySelector(".text");
+
+// 글자 모음
+const letters = [
+    "HELLO",
+    "I'M TAEWAN KIM, a UI/UX DESIGNER",
+    ""
+];
+
+// 글자 입력 속도
+const speed = 100;
+let i = 0;
+
+// 타이핑 효과
+const typing = async () => {
+    const letter = letters[i].split("");
+
+    while (letter.length) {
+        await wait(speed);
+        $text.innerHTML += letter.shift();
+    }
+
+    // 잠시 대기
+    await wait(1000);
+
+    // 지우는 효과
+    remove();
+}
+
+// 글자 지우는 효과
+const remove = async () => {
+    const letter = letters[i].split("");
+
+    while (letter.length) {
+        await wait(speed);
+
+        letter.pop();
+        $text.innerHTML = letter.join("");
+    }
+
+    // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
+    i = !letters[i + 1] ? 0 : i + 1;
+    typing();
+}
+
+// 딜레이 기능 ( 마이크로초 )
+function wait(ms) {
+    return new Promise(res => setTimeout(res, ms))
+}
+
+// 초기 실행
+setTimeout(typing, 1500);
+
+// "use strict";
+// const content = "HELLO, I`M TAEWAN KIM a UI/UX DESIGNER "
+// const text = document.querySelector(".text")
+// let index = 0;
+
+// //  function sleep(delay){
+// //      const start = new Date().getTime();
+// //      while (new Date().getTime() < start + delay);
+// //     }
+
+// function typing(){
+//     text.textContent += content[index++];
+//     if(index > content.length){
+//         text.textContent = ""
+//         index = 0;
+//         sleep(3000);
+//     }
+// }
+// setInterval(typing, 200)
